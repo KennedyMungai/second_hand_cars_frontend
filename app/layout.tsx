7f2b8b5db3933ec546d239332c63ae0dd0861228
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 12:22:19 +0300

    Imported the Header and Footer components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
