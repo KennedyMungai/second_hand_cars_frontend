a45372af8f4403ff473ebf6e11e64e8909962f28
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 12:25:56 +0300

    Added a link to the nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const Header = (props: Props) => {
-	return <div>Header</div>
+	return (
+		<nav className='flex justify-between relative items-center font-mono h-16'>
+			<Link href={'/'} className='pl-8 text-xl font-bold'>
+				Second Hand Cars
+			</Link>
+		</nav>
+	)
 }
 
 export default Header
